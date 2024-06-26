0282fac1ab3030094c28312c0c43f27cde0dab45
orilandDo <oriland2019@gmail.com>
2024-06-26 08:10:16 +0700

    new requirement 1706
    hien thi san pham trong danh sach don

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://backend2.xmht.vn',
+  apiUrl: 'http://localhost:3002',
   secretKey: 'JWT_SECRET_KEY-2023',
 };
 
